eaeeaf1b62f3f89b0c54784748481b8b05406552
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2021-09-10 13:28:44 +0200

    fixed formatting and refactored import

--- champ/node/rpc/account.rs
+++ champ/node/rpc/account.rs
@@ -1,8 +1,7 @@
-use crate::{state::ChampStateMutex};
-use pog_proto::rpc::account_server::Account;
-pub use pog_proto::rpc::account_server::AccountServer;
+use crate::state::ChampStateMutex;
+pub use pog_proto::rpc::account_server::{Account, AccountServer};
 use pog_proto::rpc::{BalanceReply, BalanceRequest};
-use pog_proto::rpc::{NextBlockHeightRequest, NextBlockHeightReply};
+use pog_proto::rpc::{NextBlockHeightReply, NextBlockHeightRequest};
 
 use derive_new::new;
 use tonic::{Request, Response, Status};
@@ -14,31 +13,53 @@ pub struct AccountService {
 
 #[tonic::async_trait]
 impl Account for AccountService {
-    async fn get_balance (&self, request: Request<BalanceRequest>) -> Result<Response<BalanceReply>, Status> {
+    async fn get_balance(
+        &self,
+        request: Request<BalanceRequest>,
+    ) -> Result<Response<BalanceReply>, Status> {
         // We must use .into_inner() as the fields of gRPC requests and responses are private
         let account_address = request.into_inner().address;
 
         let state = self.state.lock().await;
         let db_response = state.db.get_latest_block_by_account(&account_address).await;
-        let response = db_response.map_err(|_e| Status::new(tonic::Code::Internal, "internal server error"))?;
+        let response = db_response
+            .map_err(|_e| Status::new(tonic::Code::Internal, "internal server error"))?;
 
         match &response.data {
-            Some(data) => Ok(Response::new(BalanceReply {balance: data.balance})),
+            Some(data) => Ok(Response::new(BalanceReply {
+                balance: data.balance,
+            })),
             None => Err(Status::new(tonic::Code::Internal, "missing Block data")),
         }
     }
-    async fn get_next_block_height (&self, request: Request<NextBlockHeightRequest>) -> Result<Response<NextBlockHeightReply>, Status> {
+    async fn get_next_block_height(
+        &self,
+        request: Request<NextBlockHeightRequest>,
+    ) -> Result<Response<NextBlockHeightReply>, Status> {
         let account_address = request.into_inner().address;
 
         let state = self.state.lock().await;
         let db_response = state.db.get_latest_block_by_account(&account_address).await;
 
         let height = match db_response {
-            Ok(response) => response.data.as_ref().ok_or(Status::new(tonic::Code::Internal, "missing Block data"))?.height,
+            Ok(response) => {
+                response
+                    .data
+                    .as_ref()
+                    .ok_or(Status::new(tonic::Code::Internal, "missing Block data"))?
+                    .height
+            }
             Err(storage::DatabaseError::NoLastBlock) => 0,
-            _ => return Err(Status::new(tonic::Code::Internal, "couldn't get last block")),
-          };
+            _ => {
+                return Err(Status::new(
+                    tonic::Code::Internal,
+                    "couldn't get last block",
+                ))
+            }
+        };
 
-        Ok(Response::new(NextBlockHeightReply {next_height: height+1}))
+        Ok(Response::new(NextBlockHeightReply {
+            next_height: height + 1,
+        }))
     }
 }
--- champ/node/state.rs
+++ champ/node/state.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
-use tokio::sync::Mutex;
 use storage::Database;
+use tokio::sync::Mutex;
 
 #[derive(Debug)]
 pub struct ChampState {
