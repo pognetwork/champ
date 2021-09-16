a097e8d7303793e38a20b813f9490ec56b4e9624
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2021-09-16 15:21:38 +0200

    Formatting changes

--- champ/node/consensus/voting_power.rs
+++ champ/node/consensus/voting_power.rs
@@ -15,7 +15,7 @@ const AGE_WEIGHT: f64 = 1.0;
 // Month in Seconds
 const LOOKBACK_RANGE: u64 = 60 * 60 * 24 * 30;
 // 2 Months in Seconds
-const MAX_LOOKBACK_RANGE: u64 = 60 * 60 * 24 * 30 * 2; 
+const MAX_LOOKBACK_RANGE: u64 = 60 * 60 * 24 * 30 * 2;
 
 const WEEK_IN_SECONDS: f64 = 60.0 * 60.0 * 24.0 * 7.0;
 
@@ -28,7 +28,13 @@ pub async fn calculate_voting_power(state: &ChampStateMutex, account_id: String)
     let data = block.data.as_ref().ok_or(anyhow!("block data not found"))?;
 
     // Block from between lookback range and max lookback range
-    let old_block_result = db.get_latest_block_by_account_before(&account_id, block.timestamp - LOOKBACK_RANGE, block.timestamp - MAX_LOOKBACK_RANGE).await?;
+    let old_block_result = db
+        .get_latest_block_by_account_before(
+            &account_id,
+            block.timestamp - LOOKBACK_RANGE,
+            block.timestamp - MAX_LOOKBACK_RANGE,
+        )
+        .await?;
 
     // First Block from an account
     let first_block = db.get_block_by_height(&account_id, &0).await?;
@@ -39,7 +45,7 @@ pub async fn calculate_voting_power(state: &ChampStateMutex, account_id: String)
 
     // TODO: Green Adresses?
 
-    // TODO: Delegates 
+    // TODO: Delegates
 
     // Weights to change how much impact each factor should have
     let result = hresult * HEIGHT_WEIGHT + bresult * BALANCE_WEIGHT + aresult * AGE_WEIGHT;
@@ -51,7 +57,7 @@ fn balance_graph(balance: u64) -> f64 {
     // base function that is an Asymptote with Balance
     // where x is a balance type (tbd)
     // This tends towards 10, as the balance increases
-    NORMALIZED_MAX_POW / (1.0+E.powf((balance as f64/-5.0) + NORMALIZED_MAX_POW))
+    NORMALIZED_MAX_POW / (1.0 + E.powf((balance as f64 / -5.0) + NORMALIZED_MAX_POW))
 }
 
 fn tx_graph(block_height: u64, new_block: &Block, old_block: Option<&Block>) -> f64 {
@@ -59,7 +65,7 @@ fn tx_graph(block_height: u64, new_block: &Block, old_block: Option<&Block>) -> 
         Some(b) => b.timestamp,
         None => new_block.timestamp,
     };
-    // to get the time between the first and most recent block 
+    // to get the time between the first and most recent block
     // we need the minimum to not give too high power from the start
     let time = if new_block.timestamp - old_block_time < WEEK_IN_SECONDS as u64 {
         WEEK_IN_SECONDS
@@ -71,7 +77,7 @@ fn tx_graph(block_height: u64, new_block: &Block, old_block: Option<&Block>) -> 
     // https://www.geogebra.org/calculator/ymkv5ew6
     let tx_per_week = (time / block_height as f64) / WEEK_IN_SECONDS;
     // this is between 0 and 1 where plateau starts at 0.5
-    let graph_result = 1.0 / (tx_per_week/(PLATEAU_SIZE/2.0) - 1.0).powi(2 * TX_CURVE_MAX) + 1.0;
+    let graph_result = 1.0 / (tx_per_week / (PLATEAU_SIZE / 2.0) - 1.0).powi(2 * TX_CURVE_MAX) + 1.0;
     // to normalize tx graph and balanc graph
     graph_result * 10.0
 }
@@ -84,5 +90,5 @@ fn age_graph(account_age: u64) -> f64 {
     // 0.1x + 3 to allow the graph to go through 31 (month ish)
     // - 4 to shift the start
     let account_age_weeks = (account_age as f64 / WEEK_IN_SECONDS).floor();
-    (account_age_weeks + 1.0).log10() + (0.1* account_age_weeks + 3.0).sqrt() - 4.0
+    (account_age_weeks + 1.0).log10() + (0.1 * account_age_weeks + 3.0).sqrt() - 4.0
 }
