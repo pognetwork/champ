da2578d657828e6ed864d9732d791770e93cfea6
Henry <mail@henrygressmann.de>
2021-09-16 13:34:41 +0200

    add roughtime server option
    Signed-off-by: Henry <mail@henrygressmann.de>

--- champ/lib/roughtime/client.rs
+++ champ/lib/roughtime/client.rs
@@ -1,18 +1,1 @@
-use std::net::UdpSocket;
 
-use roughenough::{RtMessage, Tag};
-
-fn make_request(nonce: &[u8]) -> Vec<u8> {
-    let mut msg = RtMessage::new(1);
-    msg.add_field(Tag::NONC, nonce).unwrap();
-    msg.pad_to_kilobyte();
-
-    msg.encode().unwrap()
-}
-
-fn receive_response(sock: &mut UdpSocket) -> RtMessage {
-    let mut buf = [0; 744];
-    let resp_len = sock.recv_from(&mut buf).unwrap().0;
-
-    RtMessage::from_bytes(&buf[0..resp_len]).unwrap()
-}
--- champ/lib/roughtime/server.rs
+++ champ/lib/roughtime/server.rs
@@ -1,17 +1,44 @@
-use roughenough::config;
-use roughenough::config::ServerConfig;
+use std::net::SocketAddr;
+
+use hex;
+use roughenough::config::{MemoryConfig, ServerConfig, DEFAULT_BATCH_SIZE, DEFAULT_STATUS_INTERVAL};
+use roughenough::key::KmsProtection;
 use roughenough::server::{MioEvents, Server};
 
-pub async fn start_server(port: u16) {
-    let config = config::MemoryConfig::new(port);
-    polling_loop(Box::from(config))
-}
+pub struct RoughTime {}
+impl RoughTime {
+    pub fn new() -> RoughTime {
+        RoughTime {}
+    }
 
-pub fn polling_loop(config: Box<dyn ServerConfig>) {
-    let mut server = Server::new(config);
-    let mut events = MioEvents::with_capacity(1024);
+    pub async fn polling_loop(config: Box<dyn ServerConfig>) {
+        let mut server = Server::new(config);
+        let mut events = MioEvents::with_capacity(1024);
 
-    loop {
-        server.process_events(&mut events);
+        loop {
+            server.process_events(&mut events);
+        }
+    }
+
+    pub async fn start(self, addr: SocketAddr, enabled: bool) -> Result<(), Box<dyn std::error::Error>> {
+        if !enabled {
+            return Ok(());
+        }
+
+        let config = MemoryConfig {
+            port: addr.port(),
+            interface: addr.ip().to_string(),
+            seed: hex::decode("a32049da0ffde0ded92ce10a0230d35fe615ec8461c14986baa63fe3b3bac3db").unwrap(),
+            batch_size: DEFAULT_BATCH_SIZE,
+            status_interval: DEFAULT_STATUS_INTERVAL,
+            kms_protection: KmsProtection::Plaintext,
+            health_check_port: None,
+            client_stats: false,
+            fault_percentage: 0,
+        };
+
+        println!("starting roughtime server at {}", addr);
+        Self::polling_loop(Box::from(config)).await;
+        Ok(())
     }
 }
