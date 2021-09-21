ea4b7a19f8c44501fd03cb2ff80eafc594a03bd8
MaxiJ <Maxi.Junges@gmx.de>
2021-09-21 22:09:54 +0200

    public API prototype

--- champ/lib/p2p/lib.rs
+++ champ/lib/p2p/lib.rs
@@ -1,1 +1,45 @@
 
+
+pub async fn send_via_tcp<T: TcpSerializable>() {
+    //Not Implemented
+}
+
+pub async fn receive_via_tcp<T: TcpSerializable>() {
+    //Not Implemented
+}
+
+pub async fn send_via_udp<T: UdpSerializable>() {
+    //Not Implemented
+}
+
+pub async fn receive_via_udp<T: UdpSerializable>() {
+    //Not Implemented
+}
+
+#[tonic::async_trait]
+pub trait TcpSerializable {
+    async fn send() {}
+    async fn receive() {}
+}
+
+#[tonic::async_trait]
+pub trait UdpSerializable {
+    async fn send() {}
+    async fn receive() {}
+}
+
+struct Test {
+    name: String,
+    age: i32,
+}
+
+#[tonic::async_trait]
+impl TcpSerializable for Test {
+    async fn send() {
+        //do some Async stuff
+    }
+
+    async fn receive() {
+        //do some Async stuff
+    }
+}
