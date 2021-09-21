6874e878e84e85167eca27f621abd77d54704a5b
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2021-09-21 13:43:29 +0200

    adds create public key

--- champ/lib/crypto/curves/curve25519.rs
+++ champ/lib/crypto/curves/curve25519.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
-use ring::{rand, signature};
+use ring::{rand, signature::{self, KeyPair}};
 // https://www.youtube.com/watch?v=NF1pwjL9-DE Computerphile Vid
-// increment nr of times watched without understanding: 2
+// increment nr of times watched without understanding: 3
 
 /// Verifiy the signature of some data
 pub fn verify_signature(data: Vec<u8>, public_key: Vec<u8>, signature: Vec<u8>) -> Result<()> {
@@ -16,8 +16,15 @@ pub fn create_signature(data: Vec<u8>, private_key: Vec<u8>) -> Result<Vec<u8>> 
     Ok(key_pair.sign(&data).as_ref().to_vec())
 }
 
+/// create new private key
 pub fn generate_private_key() -> Result<Vec<u8>> {
     let rng = rand::SystemRandom::new();
     let key = signature::Ed25519KeyPair::generate_pkcs8(&rng)?;
     Ok(key.as_ref().to_vec())
+}
+
+/// create a key pair from a private key
+pub fn create_public_key(private_key: Vec<u8>) -> Result<Vec<u8>>{
+    let key_pair = signature::Ed25519KeyPair::from_pkcs8(private_key.as_ref())?;  
+    Ok(key_pair.public_key().as_ref().to_vec())
 }
