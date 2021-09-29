62d4cb774bc8eeff161b90ef97b9d10b870d7fbb
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2021-09-29 11:46:48 +0200

    adds frame to validate transactions

--- champ/node/validation/block.rs
+++ champ/node/validation/block.rs
@@ -2,7 +2,7 @@ use crate::state::ChampStateMutex;
 use anyhow::{anyhow, Result};
 use crypto::{self, curves::curve25519::verify_signature};
 use encoding::account::generate_account_address;
-use pog_proto::api::Block;
+use pog_proto::api::{Block, transaction::{TxClaim, TxDelegate, TxOpen}};
 use prost::Message;
 
 // Validate block
@@ -25,7 +25,22 @@ pub async fn validate(block: &Block, state: &ChampStateMutex) -> Result<()> {
 }
 
 // Verifies the transactions and balances
-fn verify_transactions() -> Result<()> {
+fn verify_transactions(new_block: &Block, prev_block: &Block) -> Result<()> {
+    // go through all tx in the block and do math to see new balance
+    // check against block balance
+    let new_data = new_block.data.as_ref().ok_or_else(|| anyhow!("block data not found"))?;
+    let prev_data = prev_block.data.as_ref().ok_or_else(|| anyhow!("block data not found"))?;
+
+    let mut new_balance = 0;
+    for transaction in &new_data.transactions {
+        let tx_type = transaction.data.as_ref().ok_or_else(|| anyhow!("transaction data not found"))?;
+        new_balance = match tx_type {
+            TxOpen => 0, //ignore
+            TxSend => -1, // remove money from this balance TODO: validate sender
+            TxClaim => 1, // add money to this balance TODO: validate receiver
+            TxDelegate => 0, // ignore
+        }
+    }
     unimplemented!()
 }
 
@@ -40,7 +55,7 @@ fn verify_previous_block(new_block: &Block, prev_block: &Block) -> Result<()> {
     if new_data.height == 0 {
         // if new_block is the first block in the chain
         // TODO: add some magic I guess
-        return Ok(())
+        return Ok(());
     }
     if new_data.height - 1 != prev_data.height {
         return Err(anyhow!("block height not match expected"));
