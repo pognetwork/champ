cd869b4ba7f6adfe007d93b55d99b2bafc73208a
Henry <mail@henrygressmann.de>
2021-10-12 14:53:15 +0200

    chore: add wapi & wasm modules
    Signed-off-by: Henry <mail@henrygressmann.de>

---
+++ champ/lib/wapi/README.md
@@ -0,0 +1,3 @@
+# Pog Storage API
+
+This crate contains all code necessary to interact with any pog storage backend.
---
+++ champ/lib/wasm/lib.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use wasmtime::{self, Caller, Engine, Func, Instance, Module, Store};
+
+const HELLO_WORLD_WAT: &str = r#"
+(module
+    (import "host" "hello" (func $host_hello (param i32)))
+    (func (export "hello")
+        i32.const 3
+        call $host_hello)
+)
+"#;
+
+#[derive(Default)]
+pub struct PogWasmRuntime {
+    engine: Engine,
+}
+
+impl PogWasmRuntime {
+    pub fn new() -> Self {
+        let engine = Engine::default();
+        PogWasmRuntime {
+            engine,
+        }
+    }
+
+    pub fn run(self) -> Result<()> {
+        let module = Module::new(&self.engine, HELLO_WORLD_WAT)?;
+        let mut store = Store::new(&self.engine, 4);
+        let host_hello = Func::wrap(&mut store, |caller: Caller<'_, u32>, param: i32| {
+            println!("Got {} from WebAssembly", param);
+            println!("my host state is: {}", caller.data());
+        });
+
+        let instance = Instance::new(&mut store, &module, &[host_hello.into()])?;
+        let hello = instance.get_typed_func::<(), (), _>(&mut store, "hello")?;
+
+        // And finally we can call the wasm!
+        hello.call(&mut store, ())?;
+
+        Ok(())
+    }
+}
