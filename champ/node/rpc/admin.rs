5b52f73207e3ede4fa068ced958e547887247fc5
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2021-10-20 15:35:47 +0200

    adds Admin.rs adds get_version adds get_block_pool_size adds get_node_name adds set_node_name
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

---
+++ champ/node/rpc/admin.rs
@@ -0,0 +1,117 @@
+use hyper::http::request;
+use pog_proto::rpc::admin::{
+    admin_server::Admin, Empty, GetBlockPoolSizeReply, GetChainReply, GetChainRequest, GetLogsReply, GetLogsRequest,
+    GetModeReply, GetModeRequest, GetNodeNameReply, GetNodeStatusReply, GetNodeStatusRequest, GetPeersRequest,
+    GetPeersResponse, GetPendingBlocksReply, GetPendingBlocksRequest, GetVersionResponse, SetModeReply, SetModeRequest,
+    SetNodeNameRequest, UpgradeNodeRequest, UpgradeNodeResponse,
+};
+use tonic::{Response, Status};
+
+use crate::state::ChampStateArc;
+
+#[derive(Debug)]
+pub struct AdminService {
+    pub state: ChampStateArc,
+}
+
+impl AdminService {
+    pub fn new(state: ChampStateArc) -> Self {
+        Self {
+            state,
+        }
+    }
+}
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+#[tonic::async_trait]
+impl Admin for AdminService {
+    async fn get_peers(
+        &self,
+        _request: tonic::Request<GetPeersRequest>,
+    ) -> Result<tonic::Response<GetPeersResponse>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_version(
+        &self,
+        _request: tonic::Request<Empty>,
+    ) -> Result<tonic::Response<GetVersionResponse>, tonic::Status> {
+        Ok(Response::new(GetVersionResponse {
+            version: VERSION.to_string(),
+        }))
+    }
+    async fn upgrade_node(
+        &self,
+        _request: tonic::Request<UpgradeNodeRequest>,
+    ) -> Result<tonic::Response<UpgradeNodeResponse>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_pending_blocks(
+        &self,
+        _request: tonic::Request<GetPendingBlocksRequest>,
+    ) -> Result<tonic::Response<GetPendingBlocksReply>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_block_pool_size(
+        &self,
+        _request: tonic::Request<Empty>,
+    ) -> Result<tonic::Response<GetBlockPoolSizeReply>, tonic::Status> {
+        Ok(Response::new(GetBlockPoolSizeReply {
+            length: self
+                .state
+                .blockpool_client
+                .get_queue_size()
+                .await
+                .map_err(|_| Status::new(tonic::Code::Internal, "could not get queue length"))?,
+        }))
+    }
+    async fn get_node_status(
+        &self,
+        _request: tonic::Request<GetNodeStatusRequest>,
+    ) -> Result<tonic::Response<GetNodeStatusReply>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_mode(
+        &self,
+        _request: tonic::Request<GetModeRequest>,
+    ) -> Result<tonic::Response<GetModeReply>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn set_mode(
+        &self,
+        _request: tonic::Request<SetModeRequest>,
+    ) -> Result<tonic::Response<SetModeReply>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_node_name(
+        &self,
+        _request: tonic::Request<Empty>,
+    ) -> Result<tonic::Response<GetNodeNameReply>, tonic::Status> {
+        let name = &self.state.config.read().await.admin.node_name;
+        Ok(Response::new(GetNodeNameReply {
+            name: name.to_string(),
+        }))
+    }
+    async fn set_node_name(
+        &self,
+        request: tonic::Request<SetNodeNameRequest>,
+    ) -> Result<tonic::Response<Empty>, tonic::Status> {
+        let new_name = request.into_inner().new_name;
+        let mut config = self.state.config.write().await;
+        config.admin.node_name = new_name;
+        config.write().map_err(|_| Status::new(tonic::Code::Internal, "could not update the node name"))?;
+        Ok(Response::new(Empty {}))
+    }
+    async fn get_chain(
+        &self,
+        _request: tonic::Request<GetChainRequest>,
+    ) -> Result<tonic::Response<GetChainReply>, tonic::Status> {
+        unimplemented!()
+    }
+    async fn get_logs(
+        &self,
+        _request: tonic::Request<GetLogsRequest>,
+    ) -> Result<tonic::Response<GetLogsReply>, tonic::Status> {
+        unimplemented!()
+    }
+}
--- champ/node/rpc/server.rs
+++ champ/node/rpc/server.rs
@@ -1,9 +1,13 @@
 use std::{net::SocketAddr, time::Duration};
 
 use crate::{
-    rpc::account::{AccountServer, AccountService},
+    rpc::{
+        account::{AccountServer, AccountService},
+        admin::AdminService,
+    },
     state::ChampStateArc,
 };
+use pog_proto::rpc::admin::admin_server::AdminServer;
 use tonic::transport::Server;
 
 #[derive(Debug)]
@@ -20,6 +24,7 @@ impl RpcServer {
 
     pub async fn start(&self, addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
         let account_server = AccountServer::new(AccountService::new(self.state.clone()));
+        let admin_server = AdminServer::new(AdminService::new(self.state.clone()));
         println!("starting rpc server at {}", addr);
 
         // The stack of middleware that our service will be wrapped in
@@ -29,6 +34,7 @@ impl RpcServer {
             .accept_http1(true)
             .layer(layer)
             .add_service(tonic_web::enable(account_server))
+            .add_service(tonic_web::enable(admin_server))
             .serve(addr)
             .await?;
 
