e945182ecaa6e5a41ea78b0bbab33ea5ffde114c
Malox10 <Maxi.Junges@gmx.de>
2021-10-27 18:05:14 +0200

    add function to establish an outgoing connection

--- champ/lib/p2p/lib.rs
+++ champ/lib/p2p/lib.rs
@@ -1,7 +1,7 @@
 mod main;
 
 use tokio::net::{TcpListener, TcpStream, ToSocketAddrs};
-use tokio_util::codec::{Framed, FramedRead, FramedWrite, LengthDelimitedCodec};
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 
 use bytes::{Bytes, BytesMut};
 use libp2p::futures::SinkExt;
@@ -15,10 +15,23 @@ struct Connection {
 }
 
 impl Connection {
-    //waits for a connection on that address
-    pub async fn new<T: ToSocketAddrs>(address: T) -> Result<Connection, Box<dyn std::error::Error>> {
-        let stream = Connection::connect_lol(address).await?;
-        let (mut read_half, mut write_half) = stream.into_split();
+    //waits for an incoming connection on address
+    pub async fn listen<T: ToSocketAddrs>(address: T) -> Result<Connection, Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind(address).await?;
+        // The second item contains the IP and port of the new connection.
+        let (stream, _) = listener.accept().await?;
+        Connection::connection_from_stream(stream)
+    }
+
+    //establishes a connection by connecting to address
+    pub async fn connect<T: ToSocketAddrs>(address: T) -> Result<Connection, Box<dyn std::error::Error>> {
+        let stream = TcpStream::connect(address).await?;
+        Connection::connection_from_stream(stream)
+    }
+
+    //creates a Connection from the TcpStream,
+    fn connection_from_stream(stream: TcpStream) -> Result<Connection, Box<dyn std::error::Error>> {
+        let (read_half, write_half) = stream.into_split();
         let connection = Connection {
             framed_read: FramedRead::new(read_half, LengthDelimitedCodec::new()),
             framed_write: FramedWrite::new(write_half, LengthDelimitedCodec::new()),
@@ -26,18 +39,14 @@ impl Connection {
         Ok(connection)
     }
 
-    async fn connect_lol<T: ToSocketAddrs>(address: T) -> Result<TcpStream, Box<dyn std::error::Error>> {
-        let listener = TcpListener::bind(address).await?;
-        // The second item contains the IP and port of the new connection.
-        let (stream, _) = listener.accept().await?;
-        Ok(stream)
-    }
-
+    //processes bytes into Sink and flushes.
+    //use feed/ send all batch requests more efficient into the Sink
     pub async fn write(&mut self, bytes: Bytes) -> Result<(), Box<dyn std::error::Error>> {
-        self.framed_write.send(bytes).await?; //use feed/ send all batch requests more efficient into the Sink
+        self.framed_write.send(bytes).await?;
         Ok(())
     }
 
+    //waits for connection to be able to read one frame
     pub async fn read(&mut self) -> BytesMut {
         let mut buffer: BytesMut = Default::default(); //fix this
         while let Some(Ok(bytes)) = self.framed_read.next().await {
