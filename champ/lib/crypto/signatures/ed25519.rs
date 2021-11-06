ebbf99b7913e9b8cde0167279236b3f5c9af816f
Henry <mail@henrygressmann.de>
2021-11-06 14:36:38 +0100

    improve variable names
    Signed-off-by: Henry <mail@henrygressmann.de>

--- champ/lib/crypto/signatures/ed25519.rs
+++ champ/lib/crypto/signatures/ed25519.rs
@@ -16,29 +16,29 @@ pub enum Ed25519Error {
 
 /// Verify the signature of some data
 pub fn verify_signature(data: &[u8], public_key: &[u8], data_signature: &[u8]) -> Result<(), Ed25519Error> {
-    let sk = VerificationKey::try_from(public_key).map_err(|_| Ed25519Error::VerificationError)?;
+    let verification_key = VerificationKey::try_from(public_key).map_err(|_| Ed25519Error::VerificationError)?;
     let sig_bytes: [u8; 64] = data_signature.try_into().map_err(|_| Ed25519Error::VerificationError)?;
-    sk.verify(&sig_bytes.into(), data).map_err(|_| Ed25519Error::VerificationError)?;
+    verification_key.verify(&sig_bytes.into(), data).map_err(|_| Ed25519Error::VerificationError)?;
     Ok(())
 }
 
 // /// create signature from data
 pub fn create_signature(data: &[u8], private_key: &[u8]) -> Result<[u8; 64], Ed25519Error> {
-    let sk = SigningKey::try_from(private_key).map_err(|_| Ed25519Error::VerificationError)?;
-    let sig_bytes: [u8; 64] = sk.sign(data).into();
+    let signing_key = SigningKey::try_from(private_key).map_err(|_| Ed25519Error::VerificationError)?;
+    let sig_bytes: [u8; 64] = signing_key.sign(data).into();
     Ok(sig_bytes)
 }
 
 // /// create new private key
 pub fn generate_private_key() -> Result<[u8; 32], Ed25519Error> {
-    let sk = SigningKey::new(thread_rng());
-    let priv_key: [u8; 32] = sk.into();
+    let signing_key = SigningKey::new(thread_rng());
+    let priv_key: [u8; 32] = signing_key.into();
     Ok(priv_key)
 }
 
 // /// create a public key from a private key
 pub fn create_public_key(private_key: &[u8]) -> Result<[u8; 32], Ed25519Error> {
-    let sk = SigningKey::try_from(private_key).map_err(|_| Ed25519Error::VerificationError)?;
-    let pk: [u8; 32] = VerificationKey::from(&sk).into();
+    let signing_key = SigningKey::try_from(private_key).map_err(|_| Ed25519Error::VerificationError)?;
+    let pk: [u8; 32] = VerificationKey::from(&signing_key).into();
     Ok(pk)
 }
