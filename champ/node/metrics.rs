abb143d576ec0045c500bbbc377e57998c75e50e
Henry <mail@henrygressmann.de>
2021-11-15 11:37:57 +0100

    feat: add metrics
    Signed-off-by: Henry <mail@henrygressmann.de>

---
+++ champ/node/metrics.rs
@@ -0,0 +1,44 @@
+use std::net::SocketAddr;
+
+use anyhow::Result;
+use hyper::{
+    header::CONTENT_TYPE,
+    service::{make_service_fn, service_fn},
+    Body, Request, Response, Server,
+};
+
+use prometheus::{labels, opts, register_counter, register_gauge, register_histogram_vec};
+use prometheus::{Counter, Encoder, Gauge, HistogramVec, TextEncoder};
+
+#[derive(Debug)]
+pub struct MetricsServer {}
+
+impl MetricsServer {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub async fn start(&self, addr: SocketAddr, enable: bool) -> Result<(), Box<dyn std::error::Error>> {
+        if !enable {
+            return Ok(());
+        }
+
+        let serve_future =
+            Server::bind(&addr).serve(make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(serve_req)) }));
+
+        serve_future.await?;
+        Ok(())
+    }
+}
+
+async fn serve_req(_req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let encoder = TextEncoder::new();
+
+    let metric_families = prometheus::gather();
+    let mut buffer = vec![];
+    encoder.encode(&metric_families, &mut buffer).unwrap();
+
+    let response =
+        Response::builder().status(200).header(CONTENT_TYPE, encoder.format_type()).body(Body::from(buffer)).unwrap();
+
+    Ok(response)
+}
--- docs/index.md
+++ docs/index.md
@@ -1,5 +1,5 @@
 ## Welcome to `champ`'s documentation!
 
-#### This documentation is seperated into two parts, [**developers**](./developers/setup) and [**operators**](./operators/installation).
+#### This documentation is separated into two parts, [**developers**](./developers/setup) and [**operators**](./operators/installation).
 
-The developer documentation is primarily aimed at people who already have development experience and wish to modify, contribute and interact with the project while our operator documentation provides a startingpoint for getting a node up and running and interacting with and securing it.
+The developer documentation is primarily aimed at people who already have development experience and wish to modify, contribute and interact with the project while our operator documentation provides a starting point for getting a node up and running and interacting with and securing it.
