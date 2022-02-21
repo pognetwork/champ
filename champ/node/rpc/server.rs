76c25d7fc107d740e177ef80a34a4cab405e70cd
Henry Gressmann <mail@henrygressmann.de>
2022-02-21 17:14:31 +0100

    chore: slightly improve metrics
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- champ/node/rpc/server.rs
+++ champ/node/rpc/server.rs
@@ -1,4 +1,5 @@
 use crate::auth::interceptors::interceptor_auth;
+use crate::metrics::ServiceStatus;
 use crate::rpc::block::{BlockServer, BlockService};
 use crate::rpc::node_admin::{NodeAdminServer, NodeAdminService};
 use crate::rpc::node_user::{NodeUserServer, NodeUserService};
@@ -9,6 +10,13 @@ use std::{net::SocketAddr, time::Duration};
 use tonic::transport::Server;
 use tonic::Request;
 use tracing::info;
+
+use lazy_static::lazy_static;
+use prometheus::register_int_gauge;
+
+lazy_static! {
+    static ref GRPC_HEALTH: prometheus::IntGauge = register_int_gauge!("grpc_health", "grpc server status").unwrap();
+}
 
 #[derive(Debug)]
 pub struct RpcServer {
@@ -23,6 +31,8 @@ impl RpcServer {
     }
 
     pub async fn start(&self, addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+        GRPC_HEALTH.set(ServiceStatus::Starting as i64);
+
         let (public_key, users) = {
             let cfg = &self.state.config.read().await;
             (cfg.admin.jwt_public_key.to_owned(), cfg.node_users.clone())
@@ -54,16 +64,25 @@ impl RpcServer {
         let timeout = tower::ServiceBuilder::new().timeout(Duration::from_secs(30)).into_inner();
         let server = Server::builder().accept_http1(true).layer(timeout).add_service(grpc_web.enable(block_server));
 
+        GRPC_HEALTH.set(ServiceStatus::Healthy as i64);
+
         if self.state.config.read().await.admin.enabled {
-            server
+            if let Err(e) = server
                 .add_service(grpc_web.enable(node_admin_server))
                 .add_service(grpc_web.enable(node_wallet_manager_server))
                 .add_service(grpc_web.enable(node_user))
                 .serve(addr)
-                .await?;
+                .await
+            {
+                tracing::error!("error while starting grpc server: {}", e);
+                GRPC_HEALTH.set(ServiceStatus::Broken as i64);
+            }
         } else {
             info!("admin service is disabled");
-            server.serve(addr).await?;
+            if let Err(e) = server.serve(addr).await {
+                tracing::error!("error while starting grpc server: {}", e);
+                GRPC_HEALTH.set(ServiceStatus::Broken as i64);
+            }
         }
 
         Ok(())
