7a4f05c86bf9d55094ec72f4d9f78653cfa4eb12
Henry Gressmann <mail@henrygressmann.de>
2022-03-04 15:19:16 +0100

    support latest adad specification
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- champ/lib/encoding/adad.rs
+++ champ/lib/encoding/adad.rs
@@ -1,3 +1,7 @@
+//! Synchronous and asynchronous implementations of ADAD decoders and encoders.
+//!
+//! Following the latest draft of PRC-4: https://pog.network/specification/PIPs/04-ADAD/
+
 use thiserror::Error;
 use tokio::io::{AsyncRead, AsyncReadExt};
 
@@ -14,7 +18,22 @@ pub enum ADADError {
 #[derive(Debug)]
 pub struct ADAD {
     pub associated_data: Vec<u8>,
+    pub associated_data_codec: usize,
     pub authenticated_data: Vec<u8>,
+    pub authenticated_data_codec: usize,
+}
+
+fn usize_to_varint(n: usize) -> Vec<u8> {
+    let buf = &mut unsigned_varint::encode::usize_buffer();
+    unsigned_varint::encode::usize(n, buf).into()
+}
+
+// full list on https://github.com/multiformats/multicodec/blob/master/table.csv
+pub enum Codecs {
+    Plaintext = 0x706c61,
+    Protobuf = 0x50,
+    Messagepack = 0x0201,
+    RLP = 0x60,
 }
 
 /// Encodes data as ADAD bytes
@@ -22,36 +41,48 @@ pub fn encode(data: ADAD) -> Vec<u8> {
     let aud_length = data.authenticated_data.len();
     let asd_length = data.associated_data.len();
 
-    let varint_buf = &mut unsigned_varint::encode::usize_buffer();
-    let asd_varint = unsigned_varint::encode::usize(asd_length, varint_buf);
+    let asd_varint = usize_to_varint(asd_length);
+    let associated_data_codec = usize_to_varint(data.associated_data_codec);
+    let authenticated_data_codec = usize_to_varint(data.authenticated_data_codec);
+
     let mut buf: Vec<u8> = Vec::with_capacity(asd_varint.len() + aud_length + asd_length);
 
     buf.extend(asd_varint);
+    buf.extend(associated_data_codec);
     buf.extend(&data.associated_data);
+    buf.extend(authenticated_data_codec);
     buf.extend(&data.authenticated_data);
     buf
 }
 
 /// Reads ADAD data from a reader and decodes it to associated data and authenticated data
 pub fn read<T: std::io::Read>(mut reader: T) -> Result<ADAD, ADADError> {
-    let associated_data = read_associated_data(&mut reader)?;
-
-    let mut authenticated_data = vec![];
-    reader.read_to_end(&mut authenticated_data)?;
+    let (associated_data, associated_data_codec) = read_associated_data(&mut reader)?;
+    let (authenticated_data, authenticated_data_codec) = read_authenticated_data(&mut reader)?;
 
     Ok(ADAD {
         associated_data,
+        associated_data_codec,
         authenticated_data,
+        authenticated_data_codec,
     })
 }
 
+pub fn read_authenticated_data<T: std::io::Read>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError> {
+    let codec = unsigned_varint::io::read_usize(&mut reader)?;
+    let mut authenticated_data = vec![];
+    reader.read_to_end(&mut authenticated_data)?;
+    Ok((authenticated_data, codec))
+}
+
 /// Reads associated data from a reader
-pub fn read_associated_data<T: std::io::Read>(mut reader: T) -> Result<Vec<u8>, ADADError> {
+pub fn read_associated_data<T: std::io::Read>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError> {
     let length = unsigned_varint::io::read_usize(&mut reader)?;
+    let codec = unsigned_varint::io::read_usize(&mut reader)?;
     let mut associated_data = vec![0u8; length];
 
     reader.read_exact(&mut associated_data)?;
-    Ok(associated_data)
+    Ok((associated_data, codec))
 }
 
 /// Reads ADAD data from a async reader and decodes it to associated data and authenticated data
@@ -59,27 +90,39 @@ pub async fn async_read<T>(mut reader: T) -> Result<ADAD, ADADError>
 where
     T: AsyncRead + Unpin + Send,
 {
-    let associated_data = async_read_associated_data(&mut reader).await?;
-
-    let mut authenticated_data = vec![];
-    reader.read_to_end(&mut authenticated_data).await?;
+    let (associated_data, associated_data_codec) = async_read_associated_data(&mut reader).await?;
+    let (authenticated_data, authenticated_data_codec) = async_read_authenticated_data(&mut reader).await?;
 
     Ok(ADAD {
         associated_data,
+        associated_data_codec,
         authenticated_data,
+        authenticated_data_codec,
     })
 }
 
 /// Reads associated data from a async reader
-pub async fn async_read_associated_data<T>(mut reader: T) -> Result<Vec<u8>, ADADError>
+pub async fn async_read_associated_data<T>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError>
 where
     T: AsyncRead + Unpin + Send,
 {
     let length = async_read_usize(&mut reader).await?;
+    let codec = async_read_usize(&mut reader).await?;
+
     let mut associated_data = vec![0u8; length];
 
     reader.read_exact(&mut associated_data).await?;
-    Ok(associated_data)
+    Ok((associated_data, codec))
+}
+
+pub async fn async_read_authenticated_data<T>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError>
+where
+    T: AsyncRead + Unpin + Send,
+{
+    let codec = async_read_usize(&mut reader).await?;
+    let mut authenticated_data = vec![];
+    reader.read_to_end(&mut authenticated_data).await?;
+    Ok((authenticated_data, codec))
 }
 
 /// Reads a Unsigned Varint as a usize from an AsyncRead
@@ -135,7 +178,9 @@ mod tests {
         for (associated_data, authenticated_data) in VALID_DATA {
             let encoded = adad::encode(ADAD {
                 associated_data: associated_data.to_vec(),
+                associated_data_codec: Codecs::Plaintext as usize,
                 authenticated_data: authenticated_data.to_vec(),
+                authenticated_data_codec: Codecs::Plaintext as usize,
             });
 
             let decoded = adad::read(&mut encoded.as_slice()).expect("should decode");
@@ -150,7 +195,9 @@ mod tests {
         for (associated_data, authenticated_data) in VALID_DATA {
             let encoded = adad::encode(ADAD {
                 associated_data: associated_data.to_vec(),
+                associated_data_codec: Codecs::Plaintext as usize,
                 authenticated_data: authenticated_data.to_vec(),
+                authenticated_data_codec: Codecs::Plaintext as usize,
             });
 
             let decoded = adad::async_read(&mut encoded.as_slice()).await.expect("should decode");
