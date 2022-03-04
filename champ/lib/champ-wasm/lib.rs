246a49a84bb9d2ff042ada711fa8551261ee28aa
Henry Gressmann <mail@henrygressmann.de>
2022-03-04 19:19:03 +0100

    fix: fix wasm code
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- champ/lib/champ-wasm/lib.rs
+++ champ/lib/champ-wasm/lib.rs
@@ -1,17 +1,41 @@
+#![allow(clippy::unused_unit)]
+
 use base64::encode;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Wallet {}
 
-#[allow(clippy::unused_unit)]
+#[wasm_bindgen]
+pub struct GeneratedWallet {
+    wallet_json: String,
+    wallet_address: String,
+}
+
+#[wasm_bindgen]
+impl GeneratedWallet {
+    #[wasm_bindgen(getter = json)]
+    pub fn wallet_json(&self) -> String {
+        self.wallet_json.clone()
+    }
+
+    #[wasm_bindgen(getter = address)]
+    pub fn wallet_address(&self) -> String {
+        self.wallet_address.clone()
+    }
+}
+
 #[wasm_bindgen]
 impl Wallet {
-    //#[wasm_bindgen(catch, js_name = "generate")]
-    //pub fn generate_wallet(password: String) -> Result<(String, String), JsValue> {
-    //    lulw::generate_wallet(password).map_err(|e| JsValue::from_str(&e.to_string()))
-    //}
-    //TODO fix this
+    #[wasm_bindgen(catch, js_name = "generate")]
+    pub fn generate_wallet(password: String) -> Result<GeneratedWallet, JsValue> {
+        let (wallet_json, wallet_address) =
+            lulw::generate_wallet(password).map_err(|e| JsValue::from_str(&e.to_string()))?;
+        Ok(GeneratedWallet {
+            wallet_address,
+            wallet_json,
+        })
+    }
 
     #[wasm_bindgen(catch, js_name = "unlock")]
     pub fn unlock_wallet(wallet: String, password: String) -> Result<String, JsValue> {
