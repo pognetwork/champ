cfc924e444920aec93ccae77d5278a18d1006b2d
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2022-03-07 20:37:59 +0100

    created send_request fn
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- champ/node/p2p/server.rs
+++ champ/node/p2p/server.rs
@@ -1,8 +1,14 @@
 #![allow(dead_code, unused_variables)]
 
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use crate::p2p::pogprotocol::{self, PogProtocol};
 use crate::state::ChampStateArc;
 use anyhow::Result;
+use crypto::signatures::ed25519::create_signature;
+use libp2p::noise::AuthenticKeypair;
+use pog_proto::p2p::{RequestData, RequestHeader, ResponseBody, ResponseHeader};
+use pog_proto::Message;
 
 use libp2p::{
     core::{identity, upgrade, Transport},
@@ -20,6 +26,7 @@ use super::pogprotocol::{PogBehavior, PogMessage, PogRequest, PogResponse};
 pub struct P2PServer {
     state: ChampStateArc,
     swarm: Swarm<PogBehavior>,
+    keypair: AuthenticKeypair<X25519Spec>,
 }
 
 impl P2PServer {
@@ -27,10 +34,9 @@ impl P2PServer {
         let id_keys = identity::Keypair::generate_ed25519();
         let dh_keys = Keypair::<X25519Spec>::new().into_authentic(&id_keys).unwrap();
         let peer_id = PeerId::from(id_keys.public());
-
         let pog_protocol = PogProtocol::new();
 
-        let noise = NoiseConfig::xx(dh_keys).into_authenticated();
+        let noise = NoiseConfig::xx(dh_keys.clone()).into_authenticated();
         let transp = TokioTcpConfig::new()
             .upgrade(upgrade::Version::V1)
             .authenticate(noise)
@@ -42,6 +48,7 @@ impl P2PServer {
         Self {
             state,
             swarm,
+            keypair: dh_keys,
         }
     }
 
@@ -74,7 +81,7 @@ impl P2PServer {
         }
     }
 
-    fn process_message(&self, peer: PeerId, message: PogMessage) {
+    fn process_message(&mut self, peer: PeerId, message: PogMessage) {
         match message {
             pogprotocol::RequestMessage {
                 channel,
@@ -98,6 +105,53 @@ impl P2PServer {
     //     },
     // )
 
-    fn process_request(&self, channel: ResponseChannel<PogResponse>, request: PogRequest, request_id: RequestId) {}
+    fn process_request(&mut self, channel: ResponseChannel<PogResponse>, request: PogRequest, request_id: RequestId) {
+        let header_result = RequestHeader::decode(&*request.header);
+        let header = match header_result {
+            Ok(h) => h,
+            _ => {
+                tracing::error!("could not decode header");
+                self.send_response(channel, pog_proto::p2p::response_body::Data::Failure(0));
+                return;
+            }
+        };
+
+        let data = request.data;
+    }
     fn process_response(&self, request_id: RequestId, response: PogResponse) {}
+    fn send_response(&mut self, channel: ResponseChannel<PogResponse>, response: pog_proto::p2p::response_body::Data) {
+        let data = ResponseBody {
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_secs() as u64,
+            data: Some(response),
+        }
+        .encode_to_vec();
+
+        let signature = match create_signature(&*data, self.keypair.secret().as_ref()) {
+            Ok(s) => s,
+            _ => {
+                tracing::error!("could not create signature");
+                return;
+            }
+        }
+        .to_vec();
+        let public_key = self.keypair.public().as_ref().to_vec();
+
+        let header = ResponseHeader {
+            signature,
+            public_key,
+        }
+        .encode_to_vec();
+
+        let result = self.swarm.behaviour_mut().send_response(
+            channel,
+            PogResponse {
+                data,
+                header,
+            },
+        );
+
+        if let Err(error) = result {
+            tracing::error!("error during response sending");
+        }
+    }
 }
