1969ef5b661bc8abf2aa78349fbb1a41f3a67a19
Henry Gressmann <mail@henrygressmann.de>
2022-03-07 14:31:55 +0100

    progress
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- champ/lib/encoding/adad.rs
+++ champ/lib/encoding/adad.rs
@@ -2,8 +2,8 @@
 //!
 //! Following the latest draft of PRC-4: https://pog.network/specification/PIPs/04-ADAD/
 
+use futures::io::{AsyncRead, AsyncReadExt};
 use thiserror::Error;
-use tokio::io::{AsyncRead, AsyncReadExt};
 
 #[derive(Error, Debug)]
 pub enum ADADError {
@@ -104,11 +104,12 @@ where
 /// Reads associated data from a async reader
 pub async fn async_read_associated_data<T>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError>
 where
-    T: AsyncRead + Unpin + Send,
+    T: futures::io::AsyncRead + Unpin + Send,
 {
-    let length = async_read_usize(&mut reader).await?;
-    let codec = async_read_usize(&mut reader).await?;
+    let length = unsigned_varint::aio::read_usize(&mut reader).await?;
+    let codec = unsigned_varint::aio::read_usize(&mut reader).await?;
 
+    todo!("pls add max size to prevent ddos");
     let mut associated_data = vec![0u8; length];
 
     reader.read_exact(&mut associated_data).await?;
@@ -117,47 +118,14 @@ where
 
 pub async fn async_read_authenticated_data<T>(mut reader: T) -> Result<(Vec<u8>, usize), ADADError>
 where
-    T: AsyncRead + Unpin + Send,
+    T: futures::io::AsyncRead + Unpin + Send,
 {
-    let codec = async_read_usize(&mut reader).await?;
+    let codec = unsigned_varint::aio::read_usize(&mut reader).await?;
     let mut authenticated_data = vec![];
+
+    todo!("pls add max size to prevent ddos");
     reader.read_to_end(&mut authenticated_data).await?;
     Ok((authenticated_data, codec))
-}
-
-/// Reads a Unsigned Varint as a usize from an AsyncRead
-///
-/// This is based on https://github.com/paritytech/unsigned-varint/blob/master/src/aio.rs,
-/// we've ported it to use tokio instead for simplicity
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of
-// this software and associated documentation files (the "Software"), to deal in
-// the Software without restriction, including without limitation the rights to
-// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-// the Software, and to permit persons to whom the Software is furnished to do so,
-// subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
-// OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-async fn async_read_usize<R: AsyncRead + Unpin>(mut reader: R) -> Result<usize, unsigned_varint::io::ReadError> {
-    let mut b = unsigned_varint::encode::usize_buffer();
-    for i in 0..b.len() {
-        let n = reader.read(&mut b[i..=i]).await?;
-        if n == 0 {
-            return Err(unsigned_varint::io::ReadError::Io(std::io::ErrorKind::UnexpectedEof.into()));
-        }
-        if unsigned_varint::decode::is_last(b[i]) {
-            return Ok(unsigned_varint::decode::usize(&b[..=i])?.0);
-        }
-    }
-    Err(unsigned_varint::io::ReadError::Decode(unsigned_varint::decode::Error::Overflow))
 }
 
 #[cfg(test)]
--- champ/node/p2p/pogprotocol.rs
+++ champ/node/p2p/pogprotocol.rs
@@ -1,20 +1,27 @@
 use async_trait::async_trait;
+use encoding::adad;
 use libp2p::{
-    core::{
-        upgrade::{read_length_prefixed, write_length_prefixed},
-        ProtocolName,
-    },
+    core::{upgrade::write_length_prefixed, ProtocolName},
     futures::{io, AsyncRead, AsyncWrite, AsyncWriteExt},
     request_response::{ProtocolSupport, RequestResponse, RequestResponseCodec},
 };
+use pog_proto::{p2p, Message};
 
 #[derive(Debug, Clone)]
 pub struct PogCodec();
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct PogRequest(String);
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct PogResponse(String);
+#[derive(Debug, Clone)]
+pub struct PogRequest {
+    pub header: p2p::RequestHeader,
+    pub data: Vec<u8>,
+}
+
+#[derive(Debug, Clone)]
+pub struct PogResponse {
+    pub header: p2p::ResponseHeader,
+    pub data: p2p::ResponseData,
+    pub data_raw: Vec<u8>,
+}
 
 #[derive(Debug, Clone)]
 pub struct PogProtocol();
@@ -22,6 +29,18 @@ pub struct PogProtocol();
 impl ProtocolName for PogProtocol {
     fn protocol_name(&self) -> &[u8] {
         "/pog/1".as_bytes()
+    }
+}
+
+fn invalid_data_error(msg: &str) -> std::io::Error {
+    std::io::Error::new(std::io::ErrorKind::InvalidData, msg)
+}
+
+const PROTOBUF_CODEC: u32 = adad::Codecs::Protobuf as u32;
+fn is_proto(codec: usize) -> Result<(), io::Error> {
+    match codec as u32 {
+        PROTOBUF_CODEC => Ok(()),
+        _ => Err(invalid_data_error("invalid coded")),
     }
 }
 
@@ -35,33 +54,61 @@ impl RequestResponseCodec for PogCodec {
     where
         T: AsyncRead + Unpin + Send,
     {
-        let vec = read_length_prefixed(io, 1_000_000).await?;
+        let data = adad::async_read(io).await.map_err(|_| invalid_data_error("invalid data 😔"))?;
 
-        if vec.is_empty() {
-            return Err(io::ErrorKind::UnexpectedEof.into());
-        }
+        is_proto(data.authenticated_data_codec)?;
+        is_proto(data.associated_data_codec)?;
 
-        Ok(PogRequest(String::from_utf8(vec).unwrap()))
+        let header = p2p::RequestHeader::decode(&*data.associated_data)?;
+
+        Ok(PogRequest {
+            data: data.authenticated_data,
+            header,
+        })
     }
 
     async fn read_response<T>(&mut self, _: &PogProtocol, io: &mut T) -> io::Result<Self::Response>
     where
         T: AsyncRead + Unpin + Send,
     {
-        let vec = read_length_prefixed(io, 1_000_000).await?;
+        let raw_data = adad::async_read(io).await.map_err(|_| invalid_data_error("invalid data 😔"))?;
 
-        if vec.is_empty() {
-            return Err(io::ErrorKind::UnexpectedEof.into());
-        }
+        is_proto(raw_data.authenticated_data_codec)?;
+        is_proto(raw_data.associated_data_codec)?;
 
-        Ok(PogResponse(String::from_utf8(vec).unwrap()))
+        let header = p2p::ResponseHeader::decode(&*raw_data.associated_data)?;
+        let data = p2p::ResponseData::decode(&*raw_data.authenticated_data)?;
+
+        Ok(PogResponse {
+            data,
+            data_raw: raw_data.authenticated_data,
+            header,
+        })
     }
 
-    async fn write_request<T>(&mut self, _: &PogProtocol, io: &mut T, PogRequest(data): PogRequest) -> io::Result<()>
+    async fn write_request<T>(
+        &mut self,
+        _: &PogProtocol,
+        io: &mut T,
+        PogRequest {
+            data,
+            header,
+        }: PogRequest,
+    ) -> io::Result<()>
     where
         T: AsyncWrite + Unpin + Send,
     {
-        write_length_prefixed(io, data).await?;
+        let associated_data = header.encode_to_vec();
+        let authenticated_data = data.encode_to_vec();
+
+        let buf = adad::encode(adad::ADAD {
+            associated_data,
+            associated_data_codec: adad::Codecs::Protobuf as usize,
+            authenticated_data,
+            authenticated_data_codec: adad::Codecs::Protobuf as usize,
+        });
+
+        write_length_prefixed(io, buf).await?;
         io.close().await?;
 
         Ok(())
@@ -71,12 +118,26 @@ impl RequestResponseCodec for PogCodec {
         &mut self,
         _: &PogProtocol,
         io: &mut T,
-        PogResponse(data): PogResponse,
+        PogResponse {
+            header,
+            data_raw,
+            data: _,
+        }: PogResponse,
     ) -> io::Result<()>
     where
         T: AsyncWrite + Unpin + Send,
     {
-        write_length_prefixed(io, data).await?;
+        let associated_data = header.encode_to_vec();
+        let authenticated_data = data_raw;
+
+        let buf = adad::encode(adad::ADAD {
+            associated_data,
+            associated_data_codec: adad::Codecs::Protobuf as usize,
+            authenticated_data,
+            authenticated_data_codec: adad::Codecs::Protobuf as usize,
+        });
+
+        write_length_prefixed(io, buf).await?;
         io.close().await?;
 
         Ok(())
