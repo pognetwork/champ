4feedcffe28301c46bc0de5a3c59d559cf79d671
Henry Gressmann <mail@henrygressmann.de>
2022-03-15 23:56:20 +0100

    build: add canary wasm builds
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

---
+++ scripts/js/bump-champ-wasm.js
@@ -0,0 +1,12 @@
+import { readFileSync, writeFileSync } from "fs";
+import shell from "shelljs";
+
+shell.cd('../../');
+const hash = shell.exec("git show -s --format=%h").trim();
+
+shell.cd('champ/lib/champ-wasm/pkg');
+const pkg = JSON.parse(readFileSync("./package.json"));
+let version = pkg.version.split(".").map(i => parseInt(i));
+pkg.version = `${version[0]}.${version[1]}.${version[2]}-${hash}`;
+
+writeFileSync("./package.json", JSON.stringify(pkg, null, 2), "utf-8");
