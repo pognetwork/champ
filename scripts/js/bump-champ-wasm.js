cb0cf123c56c237ec0e513b8804b4ec422b921a4
Henry Gressmann <mail@henrygressmann.de>
2022-03-16 00:06:14 +0100

    build: hopefully fix github action for wasm module
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- scripts/js/bump-champ-wasm.js
+++ scripts/js/bump-champ-wasm.js
@@ -1,10 +1,9 @@
 import { readFileSync, writeFileSync } from "fs";
 import shell from "shelljs";
 
-shell.cd('../../');
 const hash = shell.exec("git show -s --format=%h").trim();
 
-shell.cd('champ/lib/champ-wasm/pkg');
+shell.cd('../../champ/lib/champ-wasm/pkg');
 const pkg = JSON.parse(readFileSync("./package.json"));
 let version = pkg.version.split(".").map(i => parseInt(i));
 pkg.version = `${version[0]}.${version[1]}.${version[2]}-${hash}`;
