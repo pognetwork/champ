f5b569bcffd153e5f5a02a34bfe4b7488232aa76
Henry Gressmann <mail@henrygressmann.de>
2022-05-10 22:16:48 +0200

    remove rickroll
    Signed-off-by: Henry Gressmann <mail@henrygressmann.de>

--- champ/node/metrics.rs
+++ champ/node/metrics.rs
@@ -53,18 +53,7 @@ impl MetricsServer {
     }
 }
 
-lazy_static! {
-    static ref URL: String = String::from_utf8(
-        encoding::zbase32::decode("pb48ehdu8ez1675zqhz8155iqt4sr3jqcpzs4m5zcf4gg4b9qa6uc5tuqbdrcwnuptmue").unwrap(),
-    )
-    .unwrap();
-}
-
 async fn serve_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    if req.uri().to_string().contains("memes") {
-        let _ = webbrowser::open(&URL);
-    }
-
     let encoder = TextEncoder::new();
 
     let metric_families = prometheus::gather();
